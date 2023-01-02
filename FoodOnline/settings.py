26e98a2c4ae5163cae150cb714959a39e230a569
HrRadhe <harshil.iananya@gmail.com>
2023-01-02 17:57:20 +0530

    vendor profile validation

--- FoodOnline/settings.py
+++ FoodOnline/settings.py
@@ -151,6 +151,6 @@ MESSAGE_TAGS = {
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'mlhmfmcuxnyopyxb'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <harshil.iananya@gmail.com>'
