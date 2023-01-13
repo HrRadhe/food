57c561cad09cb68d7026a8f0e44eae394d306f12
HrRadhe <harshil.iananya@gmail.com>
2023-01-13 16:18:12 +0530

    dynamic tax module

--- FoodOnline/static/js/custom.js
+++ FoodOnline/static/js/custom.js
@@ -118,10 +118,12 @@ $(document).ready(function () {
                     //cart subtotal, grand total, tax
                     applyCartAmount(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
 
                     )
+
+                    // console.log(response.cart_amount['tax_dict']);
                 }
             },
             // error: function(response) { 
@@ -185,7 +187,7 @@ $(document).ready(function () {
                     //cart subtotal, grand total, tax
                     applyCartAmount(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
 
                     )
@@ -234,7 +236,7 @@ $(document).ready(function () {
                     //cart subtotal, grand total, tax
                     applyCartAmount(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
 
                     )
@@ -261,12 +263,29 @@ $(document).ready(function () {
             document.getElementById('empty-cart').style.display = 'block';
     }
 
+    // //apply cart amount
+    // function applyCartAmount(subtotal, tax, grand_total) {
+    //     if (window.location.pathname == '/cart/') {
+    //         $('#subtotal').html(subtotal)
+    //         $('#tax').html(tax)
+    //         $('#grand_total').html(grand_total)
+    //     }
+    // }
     //apply cart amount
-    function applyCartAmount(subtotal, tax, grand_total) {
+    // // after video 157
+    function applyCartAmount(subtotal, tax_dict, grand_total) {
         if (window.location.pathname == '/cart/') {
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#grand_total').html(grand_total)
+
+            // console.log(tax_dict)
+            for(key1 in tax_dict) {
+                // console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Cart
+from .models import Cart,Tax
 from menu.models import FoodItem
 
 def get_cart_counter(request):
@@ -21,12 +21,30 @@ def get_cart_amount(request):
     subtotal = 0
     tax = 0
     grand_total = 0
+    tax_dict = {}
     if request.user.is_authenticated:
         cart_item = Cart.objects.filter(user=request.user)
         for item in cart_item:
             fooditem = FoodItem.objects.get(id=item.fooditem.id)
             subtotal += (fooditem.price * item.quantity)
 
+        # add leter video no. 155
+        get_tax = Tax.objects.filter(is_active = True)
+        # print(get_tax)
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage * subtotal)/100, 2)
+            # print(tax_amount, tax_percentage, tax_type)
+            # # store in dict
+            tax_dict.update({tax_type : {str(tax_percentage) : tax_amount}})
+
+        # print(tax_dict)
+        tax = 0
+        for key in tax_dict.values():
+            # print(key)
+            for x in key.values():
+                tax += x
         grand_total = subtotal + tax
 
-    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total ,tax_dict = tax_dict)
